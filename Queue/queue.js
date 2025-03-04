ce295925f4b8b42b77f3ff1746be403a179c2cc3
Nadeem-pc <nadeempc777@gmail.com>
2025-03-04 09:09:29 +0530

    Implemented queue using Linked list

--- Queue/queue.js
+++ Queue/queue.js
@@ -40,3 +40,67 @@ queue.enqueue(20)
 queue.enqueue(30)
 queue.dequeue()
 queue.print()
+
+
+
+// Queue Implementation using a Linked List
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class QueueUsingLL {
+    constructor() {
+        this.front = null;
+        this.rear = null;
+        this.length = 0;
+    }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty()) {
+            this.front = this.rear = newNode;
+        } else {
+            this.rear.next = newNode;
+            this.rear = newNode;
+        }
+        this.length++;
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return "Queue is empty";
+        }
+        const dequeuedValue = this.front.value;
+
+        this.front = this.front.next;
+        this.length--;
+
+        if (this.isEmpty()) {
+            this.rear = null; 
+        }
+        return dequeuedValue;
+    }
+
+    peek() {
+        return this.isEmpty() ? "Queue is empty" : this.front.value;
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    size() {
+        return this.length;
+    }
+}
+
+let queueUsingLL = new QueueUsingLL();
+queueUsingLL.enqueue(10);
+queueUsingLL.enqueue(20);
+queueUsingLL.enqueue(30);
+console.log("Front element:", queueUsingLL.peek()); 
+console.log("Dequeued:", queueUsingLL.dequeue());  
+console.log("Front element:", queueUsingLL.peek()); 
