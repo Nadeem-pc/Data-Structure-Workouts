7556acf36468388a8315be71c27e824023e25b21
Nadeem-pc <nadeempc777@gmail.com>
2025-03-19 09:30:53 +0530

    Change in file structure

--- Tree/Bst/bstWorkouts.js
+++ Tree/Bst/bstWorkouts.js
@@ -5,6 +5,7 @@ class Node{
         this.right = null;
     }
 }
+
 class BinarySearchTree{
     constructor(){
         this.root = null;
@@ -34,92 +35,6 @@ class BinarySearchTree{
                 this.insertNode(root.right, newNode)
             }
         }
-    }
-    search(root,value){
-        if(!root){
-            return false
-        }
-        if(root.value === value){
-            return true
-        }
-        if(root.value > value){
-            return this.search(root.left,value)
-        }
-        return this.search(root.right,value)
-    }
-    preOrder(root){
-        if(root){
-            console.log(root.value);
-            this.preOrder(root.left);
-            this.preOrder(root.right)
-        }
-    }
-    inOrder(root){
-        if(root){
-            this.inOrder(root.left);
-            console.log(root.value);
-            this.inOrder(root.right)
-        }
-    }
-    postOrder(root){
-        if(root){
-            this.postOrder(root.left);
-            this.postOrder(root.right)
-            console.log(root.value)
-        }
-    }
-    levelOrder(){
-        const queue = [];
-        queue.push(this.root);
-        while(queue.length){
-            let curr = queue.shift();
-            console.log(curr.value)
-            if(curr.left){
-                queue.push(curr.left);
-            }
-            if(curr.right){
-            queue.push(curr.right)      
-            }
-        }
-    }
-    min(root){
-        if(!root.left){
-            return root.value;
-        }else{
-            return this.min(root.left)
-        }
-    }
-    max(root){
-        if(!root.right){
-            return root.value;
-        }else{
-            return this.max(root.right)
-        }
-    }
-    delete(value){
-        this.root = this.deleteNode(this.root,value);
-    }
-    deleteNode(root,value){
-        if(!root) return null
-
-        if(value<root.value){
-            root.left = this.deleteNode(root.left,value)
-        }else if(value > root.value){
-            root.right = this.deleteNode(root.right,value)
-        }else{
-            if(!root.left && !root.right){
-                return null;
-            }
-            if(!root.left){
-                return root.right;
-            }else if(!root.right){
-                return root.left
-            }
-            root.value = this.min(root.right);
-            root.right = this.deleteNode(root.right,root.value)
-        }
-        return root
-        
     }
     findClosest(target) {
         let curr = this.root;
@@ -305,26 +220,6 @@ bst.insert(5);
 bst.insert(15);
 bst.insert(7);
 bst.insert(3);
-
-console.log(bst.search(bst.root,11))
-console.log(bst.search(bst.root,15))
-
-console.log('Preorder traversal :')
-bst.preOrder(bst.root)
-
-console.log('Inorder traversal :')
-bst.inOrder(bst.root)
-
-console.log('Postorder traversal :')
-bst.postOrder(bst.root);
-
-console.log('Bfs traversal :')
-bst.levelOrder()
-
-bst.delete(15)
-
-console.log('Minimum value is:',bst.min(bst.root))
-console.log('Maximum value is:',bst.max(bst.root))
 
 console.log('Closest value of 6',bst.findClosest(6))
 
